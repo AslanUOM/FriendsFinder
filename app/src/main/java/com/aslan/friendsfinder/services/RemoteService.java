7779d4ab81530ecb406eefbbdcbaab09e0c19f32
vishnuvathsan <vishnuvathsan@users.noreply.github.com>
2015-11-04 19:28:34 +0530

    Message passing interface for PLUGIN and APP is added

--- app/src/main/java/com/aslan/friendsfinder/services/RemoteService.java
+++ app/src/main/java/com/aslan/friendsfinder/services/RemoteService.java
@@ -2,15 +2,38 @@ package com.aslan.friendsfinder.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.widget.Toast;
 
 public class RemoteService extends Service {
-    public RemoteService() {
-    }
+    static final int SAY_BYE = 0;
+    static final int SAY_SEE_YOU = 1;
+    Messenger mMessenger = new Messenger(new RemoteServiceHandler());
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public IBinder onBind(Intent arg0) {
+        // TODO Auto-generated method stub
+        return mMessenger.getBinder();
     }
+
+    private class RemoteServiceHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case SAY_BYE:
+                    Toast.makeText(getApplicationContext(), "Bye from PLUGIN @ APP", Toast.LENGTH_LONG).show();
+                    break;
+                case SAY_SEE_YOU:
+                    Toast.makeText(getApplicationContext(), "See you from PLUGIN @ APP", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    }
+
 }
