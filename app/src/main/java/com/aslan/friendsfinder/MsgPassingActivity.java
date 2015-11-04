5d8ef74ea1299e9a5787e528621df7c8f723d9bd
vishnuvathsan <vishnuvathsan@users.noreply.github.com>
2015-11-04 19:26:30 +0530

    message passing interface between plugin and app added

---
+++ app/src/main/java/com/aslan/friendsfinder/MsgPassingActivity.java
@@ -0,0 +1,13 @@
+package com.aslan.friendsfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MsgPassingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msg_passing);
+    }
+}
